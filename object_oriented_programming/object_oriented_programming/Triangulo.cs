f3732f22a8fb4891985b902a61893406b0ddaf9f
Renata Guedes <renataleitesg@gmail.com>
2020-10-07 21:35:14 +0100

    41. creating a method to obtain the reuse and delegation benefits

--- object_oriented_programming/object_oriented_programming/Triangulo.cs
+++ object_oriented_programming/object_oriented_programming/Triangulo.cs
@@ -10,5 +10,9 @@ namespace object_oriented_programming
         public double B;
         public double C;
 
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p-A) * (p - B) * (p - C));
+        }
     }
 }
