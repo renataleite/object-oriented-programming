25094d62f7cf899b874024a05e905c6ab85996e5
Renata Guedes <renataleitesg@gmail.com>
2020-10-08 08:10:21 +0100

    42. Starting to solve a second example problem

---
+++ object_oriented_programming/stock/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stock
+{
+    class Product
+    {
+        public string Name;
+        public double Price;
+        public int Stoque;
+
+        public double TotalStock()
+        {
+            return Price * Stoque; ;
+        }
+
+        public void AddProducts(int quantity)
+        {
+            Stoque = Stoque + quantity;
+        }
+
+        public void RemoveProducts(int quantity)
+        {
+            Stoque = Stoque - quantity;
+        }
+    }
+}
---
+++ object_oriented_programming/stock/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1;
+
+            p1 = new Product();
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            p1.Name = Console.ReadLine();
+            Console.Write("Preco: ");
+            p1.Price = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: ");
+            p1.Stoque = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p1.Name + ", $" + p1.Price.ToString("F2") + ", " + p1.Stoque + " unidades" + ", Total: §" + p1.TotalStock().ToString("F2"));
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quantity = int.Parse(Console.ReadLine());
+            p1.AddProducts(quantity);
+            Console.WriteLine("Dados atualizados: " + p1.Name + ", $" + p1.Price.ToString("F2") + ", " + p1.Stoque + " unidades" + ", Total: §" + p1.TotalStock().ToString("F2"));
+            Console.Write("Digite o número de produtos a ser removido ao estoque: ");
+            quantity = int.Parse(Console.ReadLine());
+            p1.RemoveProducts(quantity);
+            Console.WriteLine("Dados atualizados: " + p1.Name + ", $" + p1.Price.ToString("F2") + ", " + p1.Stoque + " unidades" + ", Total: §" + p1.TotalStock().ToString("F2"));
+
+
+
+
+        }
+    }
+}
