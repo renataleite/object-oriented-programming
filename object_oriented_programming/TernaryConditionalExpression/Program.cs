841165fad7aedef45587af0bfe8869984a160de7
Renata Guedes <renataleitesg@gmail.com>
2020-10-20 15:20:43 +0100

    93. Special topics in C # - PART 1

---
+++ object_oriented_programming/TernaryConditionalExpression/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TernaryConditionalExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.5;
+            Console.WriteLine(desconto);
+        }
+    }
+}
