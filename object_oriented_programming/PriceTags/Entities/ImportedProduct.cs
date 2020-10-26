b66704ff43d994251f92483e5467f010e44024e5
Renata Guedes <renataleitesg@gmail.com>
2020-10-26 13:04:52 +0000

    133. Proposed exercise (inheritance and polymorphism)

---
+++ object_oriented_programming/PriceTags/Entities/ImportedProduct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceTags.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustmsFee { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double custmsFee) : base(name, price)
+        {
+            CustmsFee = custmsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustmsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " $ " + TotalPrice().ToString("F2") + "(Customs fee: $" + CustmsFee.ToString("F2") + ")";
+        }
+    }
+}
---
+++ object_oriented_programming/PriceTags/Program.cs
@@ -0,0 +1,53 @@
+﻿using PriceTags.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace PriceTags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Product> list= new List<Product>();
+
+            Console.WriteLine("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.WriteLine("Common, used or imported (c/u/i)?");
+                char type = char.Parse(Console.ReadLine());
+                Console.WriteLine("Name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Price:");
+                double price = double.Parse(Console.ReadLine());
+
+                if (type == 'i')
+                {
+                    Console.WriteLine("Customs fee:");
+                    double customs = double.Parse(Console.ReadLine());
+                    list.Add(new ImportedProduct(name, price, customs));
+                }
+                else if (type == 'u')
+                {
+                    Console.WriteLine("Manufacture date (DD/MM/YYYY):");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, manufactureDate));
+                }
+                else
+                {
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine("PRICE TAGS: ");
+
+            foreach (Product prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+
+        }
+    }
+}
